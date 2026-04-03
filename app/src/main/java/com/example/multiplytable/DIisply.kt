39646d3186bd08c3c42e2184bd876cf7f4b1015f
youssefelshazly323-debug <youssefelshazly323@gmail.com>
2026-04-03 17:03:22 +0200

    Added multiplication table display logic

--- app/src/main/java/com/example/multiplytable/DIisply.kt
+++ app/src/main/java/com/example/multiplytable/DIisply.kt
@@ -1,5 +1,6 @@
 package com.example.multiplytable
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DIisply : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,8 +20,33 @@ class DIisply : AppCompatActivity() {
         //getting the number from the intent
         val tablestring: String? = bundle?.getString("tablenumber")
         //converting the string back to an integer
-        val tableNumber = tablestring?.toInt()
+        val tableNumber = tablestring!!.toInt()
+
+        //display the heading so we can see the value in the action
         val multiplytable = findViewById<TextView>(R.id.textView)
+        multiplytable.text = "$tableNumber × table"
+        //start with the heading and add two new lines
+        var tableDisplay = "$tableNumber × table\n\n"
+        //lopp 10 numbers and add them to the string
+
+        //set the string onto the display
+
+        //declare the control variable
+        var counter = 2
+        while (counter <= 10) {
+            if (counter == 6) {
+                continue
+            }
+
+            val answer = tableNumber * counter
+            tableDisplay += "$tableNumber × $counter = $answer\n"
+
+            counter++
+        }
+
+
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- app/src/main/java/com/example/multiplytable/MainActivity.kt
+++ app/src/main/java/com/example/multiplytable/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.multiplytable
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,21 +15,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        // created variables for the button and the input
 
+        val resultTxt = findViewById<TextView>(R.id.resultTxt)
         val button = findViewById<Button>(R.id.button)
-        val numInout = findViewById<EditText>(R.id.editTextNumber)
-        // created a listener for the button
+        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
+
         button.setOnClickListener {
-         // created an intent to go to the next activity
-
-            val intent = Intent(this, DIisply::class.java)
-            // transfer the number to the next activity
-            intent.putExtra("number", numInout.text.toString())
-
-            startActivity(intent)
+            val input = editTextNumber.text.toString()
+            if (input.isNotEmpty()) {
+                val number = input.toInt()
+                val result = StringBuilder()
+                for (i in 1..10) {
+                    result.append("$number x $i = ${number * i}\n")
+                }
+                resultTxt.text = result.toString()
+                resultTxt.visibility = View.VISIBLE
+            } else {
+                editTextNumber.error = "Please enter a number"
+            }
         }
-        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
